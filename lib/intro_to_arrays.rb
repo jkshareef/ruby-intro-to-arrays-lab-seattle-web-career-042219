4e976727b95e4cd3bba11a22029cc1adab6c6593
Jamal Shareef <theepie@hotmail.com>
2019-04-02 02:22:19 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,21 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  [1. 1]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
